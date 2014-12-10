4b5027205c93c6c5fd26ba9ce56d3f138a15877e
jonschlinkert <dev@sellside.com>
2014-12-09 23:53:16 -0500

    update tests with async

--- test.js
+++ test.js
@@ -10,38 +10,68 @@
 var should = require('should');
 var handlebars = require('handlebars');
 var _ = require('lodash');
-var concatHelper = require('./');
+var concat = require('./');
 
-describe('concat helper', function () {
-  it('should return the contents of a file:', function () {
-    concatHelper('fixtures/a.txt').should.equal('AAA');
-  });
+describe('async', function () {
+  describe('concat helper', function () {
+    it('should return the contents of a file:', function (done) {
+      concat('fixtures/a.txt', function  (err, content) {
+        content.should.equal('AAA');
+        done();
+      });
+    });
 
-  it('should return the concatenated contents of a list of files:', function () {
-    concatHelper('fixtures/*.txt').should.equal('AAA\nBBB\nCCC');
+    it('should return the concatenated contents of a list of files:', function (done) {
+      concat('fixtures/*.txt', function  (err, content) {
+        content.should.equal('AAA\nBBB\nCCC');
+        done();
+      });
+    });
+
+    it('should use a custom separator:', function (done) {
+      concat('fixtures/*.txt', {sep: '~~'}, function  (err, content) {
+        content.should.equal('AAA~~BBB~~CCC');
+        done();
+      });
+    });
   });
 });
 
-describe('handlebars:', function () {
-  it('should work as a handlebars helper:', function () {
-    handlebars.registerHelper('concat', concatHelper);
-    handlebars.compile('{{concat "fixtures/*.txt"}}')().should.equal('AAA\nBBB\nCCC');
-  });
-});
+describe('sync', function () {
+  describe('concat helper', function () {
+    it('should return the contents of a file:', function () {
+      concat.sync('fixtures/a.txt').should.equal('AAA');
+    });
 
-describe('lodash:', function () {
-  it('should work as a lodash mixin:', function () {
-    _.mixin({concat: concatHelper});
-    _.template('<%= _.concat("fixtures/*.txt") %>', {}).should.equal('AAA\nBBB\nCCC');
-  });
+    it('should return the concatenated contents of a list of files:', function () {
+      concat.sync('fixtures/*.txt').should.equal('AAA\nBBB\nCCC');
+    });
 
-  it('should work when passed to lodash on the context:', function () {
-    var settings = {imports: {concat: concatHelper}};
-    _.template('<%= concat("fixtures/*.txt") %>', {concat: concatHelper}).should.equal('AAA\nBBB\nCCC');
+    it('should use a custom separator:', function () {
+      concat.sync('fixtures/*.txt', {sep: '~'}).should.equal('AAA~BBB~CCC');
+    });
   });
 
-  it('should work as a lodash import:', function () {
-    var settings = {imports: {concat: concatHelper}};
-    _.template('<%= concat("fixtures/*.txt") %>', {}, settings).should.equal('AAA\nBBB\nCCC');
+  describe('handlebars:', function () {
+    it('should work as a handlebars helper:', function () {
+      handlebars.registerHelper('concat', concat.sync);
+      handlebars.compile('{{concat "fixtures/*.txt"}}')().should.equal('AAA\nBBB\nCCC');
+    });
+  });
+
+  describe('lodash:', function () {
+    it('should work as a lodash mixin:', function () {
+      _.mixin({concat: concat.sync});
+      _.template('<%= _.concat("fixtures/*.txt") %>', {}).should.equal('AAA\nBBB\nCCC');
+    });
+
+    it('should work when passed to lodash on the context:', function () {
+      _.template('<%= concat("fixtures/*.txt") %>', {concat: concat.sync}).should.equal('AAA\nBBB\nCCC');
+    });
+
+    it('should work as a lodash import:', function () {
+      var settings = {imports: {concat: concat.sync}};
+      _.template('<%= concat("fixtures/*.txt") %>', {}, settings).should.equal('AAA\nBBB\nCCC');
+    });
   });
 });
